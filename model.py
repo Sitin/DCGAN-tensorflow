95b43a31fbec1a6e914bed24dccd78a40603c6f3
Mikhail Zyatin <mikhail.zyatin@gmail.com>
2017-01-27 01:50:26 +0200

    Increase epoch digit size.

--- model.py
+++ model.py
@@ -292,7 +292,7 @@ class DCGAN(object):
               }
             )
             save_images(samples, [8, 8],
-                  './{}/train_{:02d}_{:04d}.png'.format(config.sample_dir, epoch, idx))
+                  './{}/train_{:04d}_{:04d}.png'.format(config.sample_dir, epoch, idx))
             print("[Sample] d_loss: %.8f, g_loss: %.8f" % (d_loss, g_loss)) 
           else:
             try:
